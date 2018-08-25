f11278fd34b6090d4f71131873b30e5e7ac7f62a
Radek Fryšták <geniv.radek@gmail.com>
2018-08-25 18:28:48 +0200

    update extension

--- src/Bridges/Nette/Extension.php
+++ src/Bridges/Nette/Extension.php
@@ -3,6 +3,39 @@
 namespace AdminElement\Bridges\Nette;
 
 use AdminElement\AdminElement;
+use AdminElement\Elements\CheckboxElement;
+use AdminElement\Elements\DateElement;
+use AdminElement\Elements\DatePickerElement;
+use AdminElement\Elements\DateTimeElement;
+use AdminElement\Elements\DateTimePickerElement;
+use AdminElement\Elements\EditorElement;
+use AdminElement\Elements\FkSelectElement;
+use AdminElement\Elements\ForeignCheckboxElement;
+use AdminElement\Elements\ForeignDateTimeElement;
+use AdminElement\Elements\ForeignDateTimePickerElement;
+use AdminElement\Elements\ForeignEditorElement;
+use AdminElement\Elements\ForeignFkPkElement;
+use AdminElement\Elements\ForeignFkWhereElement;
+use AdminElement\Elements\ForeignHiddenElement;
+use AdminElement\Elements\ForeignLabelElement;
+use AdminElement\Elements\ForeignPositionElement;
+use AdminElement\Elements\ForeignSelectElement;
+use AdminElement\Elements\ForeignTextAreaElement;
+use AdminElement\Elements\ForeignTextElement;
+use AdminElement\Elements\ForeignUploadElement;
+use AdminElement\Elements\ForeignUploadImageElement;
+use AdminElement\Elements\HiddenElement;
+use AdminElement\Elements\LabelElement;
+use AdminElement\Elements\PasswordElement;
+use AdminElement\Elements\PositionElement;
+use AdminElement\Elements\RadioElement;
+use AdminElement\Elements\SelectElement;
+use AdminElement\Elements\TextAreaElement;
+use AdminElement\Elements\TextElement;
+use AdminElement\Elements\TimeElement;
+use AdminElement\Elements\TimePickerElement;
+use AdminElement\Elements\UploadElement;
+use AdminElement\Elements\UploadImageElement;
 use AdminElement\WrapperSection;
 use Nette\DI\CompilerExtension;
 
@@ -17,7 +50,41 @@ class Extension extends CompilerExtension
 {
     /** @var array default values */
     private $defaults = [
-        'elements' => [],
+        'elements' => [
+            'label'                 => LabelElement::class,
+            'hidden'                => HiddenElement::class,
+            'text'                  => TextElement::class,
+            'textarea'              => TextAreaElement::class,
+            'editor'                => EditorElement::class,
+            'password'              => PasswordElement::class,
+            'date'                  => DateElement::class,
+            'datepicker'            => DatePickerElement::class,
+            'time'                  => TimeElement::class,
+            'timepicker'            => TimePickerElement::class,
+            'datetime'              => DateTimeElement::class,
+            'datetimepicker'        => DateTimePickerElement::class,
+            'checkbox'              => CheckboxElement::class,
+            'position'              => PositionElement::class,
+            'upload'                => UploadElement::class,
+            'uploadimage'           => UploadImageElement::class,
+            'radio'                 => RadioElement::class,
+            'select'                => SelectElement::class,
+            'fkselect'              => FkSelectElement::class,
+            'foreignfkpk'           => ForeignFkPkElement::class,
+            'foreignfkwhere'        => ForeignFkWhereElement::class,
+            'foreignlabel'          => ForeignLabelElement::class,
+            'foreignhidden'         => ForeignHiddenElement::class,
+            'foreigntext'           => ForeignTextElement::class,
+            'foreigntextarea'       => ForeignTextAreaElement::class,
+            'foreigneditor'         => ForeignEditorElement::class,
+            'foreignselect'         => ForeignSelectElement::class,
+            'foreignupload'         => ForeignUploadElement::class,
+            'foreignuploadimage'    => ForeignUploadImageElement::class,
+            'foreigndatetime'       => ForeignDateTimeElement::class,
+            'foreigndatetimepicker' => ForeignDateTimePickerElement::class,
+            'foreigncheckbox'       => ForeignCheckboxElement::class,
+            'foreignposition'       => ForeignPositionElement::class,
+        ],
     ];
 
 
